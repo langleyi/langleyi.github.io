f5f7e6ee730f5e7165b71fa37f614437c81a12a7
Isaac Langley <langley.isaac5@gmail.com>
2018-02-08 17:32:40 +0000

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -19,7 +19,7 @@ var eyes = [
     'radius' : 4
   },
          { 
-    'centerX' : window.innerWidth - 128,
+    'centerX' : window.innerWidth - 120,
     'centerY' : window.innerHeight - 144,
     'radius' : 4
   },
